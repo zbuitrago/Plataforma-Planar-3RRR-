59aeafab5b27f172c6fa92fcc5b9f552204fb9a3
AndresDiaz1 <andiazvar@gmail.com>
2015-04-21 15:48:23 -0500

    Se adjunta la carpeta del proyecto en Unity
    La carpeta se llama RRRplanar, allí se ecnuentra cargado el modelo 3D de
    la base, además de movimientos de giro de los brazos y antebrazos,
    respecto a sus ejes de rotación

---
+++ RRRplanar/Assets/Scripts/rotationrespectto.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotationrespectto : MonoBehaviour {
+
+	public GameObject link1;
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position=new Vector3(link1.transform.position.x,0f,link1.transform.position.z);
+		transform.
+
+		transform.RotateAround (new Vector3 (link1.transform.position.x, 0f, link1.transform.position.z), new Vector3 (0.0f, 1.0f, 0.0f), 55.0f * Time.deltaTime);
+		//transform.Rotate (new Vector3 (0, 5, 0) * Time.deltaTime);
+	}
+}
