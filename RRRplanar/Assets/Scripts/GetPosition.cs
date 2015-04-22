516d98aafa5f8cffde19822424a5a2082f682287
AndresDiaz1 <andiazvar@gmail.com>
2015-04-22 13:51:13 -0500

    Crea esferas en punto final deseado y .apk nuevo

--- RRRplanar/Assets/Scripts/GetPosition.cs
+++ RRRplanar/Assets/Scripts/GetPosition.cs
@@ -17,16 +17,30 @@ public class GetPosition : MonoBehaviour {
 		if (Input.GetMouseButton (0) == true) {
 			Vector3 point=CastRayToWorld();
 			setPointToTextField(point);
+			CreateSphere(point);
 		}
 	}
-
+	
 	private Vector3 CastRayToWorld(){
 		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-		Vector3 point= ray.origin+(ray.direction*distance);
-		return point;
+		RaycastHit hit;
+		Vector3 worldPos=Vector3.zero;
+		if (Physics.Raycast (ray, out hit, 1000f)) {
+			worldPos = hit.point;
+		} 
+		return worldPos;
 	}
 
 	private void setPointToTextField(Vector3 point){
+		point.y = 0;
 		position.text=point.ToString();
+	}
+
+	private void CreateSphere(Vector3 point){
+		if(point!=Vector3.zero){
+			GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+			sphere.transform.position = new Vector3 (point.x,0.0f,point.z);
+			sphere.transform.localScale = new Vector3 (0.5f,0.5f,0.5f);
+		}
 	}
 }
