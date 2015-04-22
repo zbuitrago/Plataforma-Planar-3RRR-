9dc6c602718a0a351f9e649066e9d44ac591285e
AndresDiaz1 <andiazvar@gmail.com>
2015-04-22 12:42:09 -0500

    Detecta posicion final deseada y .apk nuevo

---
+++ RRRplanar/Assets/Scripts/GetPosition.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GetPosition : MonoBehaviour {
+
+	public float distance=4.5f;
+	public Text position;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton (0) == true) {
+			Vector3 point=CastRayToWorld();
+			setPointToTextField(point);
+		}
+	}
+
+	private Vector3 CastRayToWorld(){
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		Vector3 point= ray.origin+(ray.direction*distance);
+		return point;
+	}
+
+	private void setPointToTextField(Vector3 point){
+		position.text=point.ToString();
+	}
+}
