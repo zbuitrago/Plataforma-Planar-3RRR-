5a56f78d5dc47be9b1ce9e2f9ae00bb99a085ca5
AndresDiaz1 <andiazvar@gmail.com>
2015-04-22 10:24:34 -0500

    Mejora movimiento de la camara
    Ahora en PC se mueve al hacer click derecho y desplazar el puntero.
    Y en Android se debe colocar 2 dedos sobre la pantalla para poder girar

--- RRRplanar/Assets/Standard Assets/Scripts/Camera Scripts/MouseOrbit.js
+++ RRRplanar/Assets/Standard Assets/Scripts/Camera Scripts/MouseOrbit.js
@@ -23,7 +23,7 @@ function Start () {
 }
 
 function LateUpdate () {
-    if (target && Input.GetMouseButton(0)==true) {
+    if (target && Input.GetMouseButton(1)==true) {
         x += Input.GetAxis("Mouse X") * xSpeed * 0.02;
         y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02;
  		
