af729a1bd023ee41f59a220bba3b527bb43c25d0
AndresDiaz1 <andiazvar@gmail.com>
2015-04-22 10:48:22 -0500

    Zoom en Android y .Apk actual
    Se hace Zoom al hacer pinch sobre la pantalla

--- RRRplanar/Assets/Standard Assets/Scripts/Camera Scripts/MouseOrbit.js
+++ RRRplanar/Assets/Standard Assets/Scripts/Camera Scripts/MouseOrbit.js
@@ -11,6 +11,10 @@ private var x = 0.0;
 private var y = 0.0;
 private var zoomStep=3;
 
+public var perspectiveZoomSpeed : float = 0.5f;        // The rate of change of the field of view in perspective mode.
+public var orthoZoomSpeed : float = 0.5f;        // The rate of change of the orthographic size in orthographic mode.
+
+
 @script AddComponentMenu("Camera-Control/Mouse Orbit")
 
 function Start () {
@@ -38,6 +42,53 @@ function LateUpdate () {
     }
     
     transform.Translate(Vector3.forward * (Input.GetAxis("Mouse ScrollWheel"))*zoomStep);
+    
+    #if UNITY_ANDROID || UNITY_IOS	
+    AndroidPinchZoom();
+    #endif
+    
+    
+    
+    
+}
+
+
+function AndroidPinchZoom(){
+	if (Input.touchCount == 2)
+    {
+        // Store both touches.
+        var touchZero = Input.GetTouch(0);
+        var touchOne = Input.GetTouch(1);
+
+        // Find the position in the previous frame of each touch.
+        var touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+        var touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+        // Find the magnitude of the vector (the distance) between the touches in each frame.
+        var prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+        var touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+        // Find the difference in the distances between each frame.
+        var deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+        // If the camera is orthographic...
+        if (camera.isOrthoGraphic)
+        {
+            // ... change the orthographic size based on the change in distance between the touches.
+            camera.orthographicSize += deltaMagnitudeDiff * orthoZoomSpeed;
+
+            // Make sure the orthographic size never drops below zero.
+            camera.orthographicSize = Mathf.Max(camera.orthographicSize, 0.1f);
+        }
+        else
+        {
+            // Otherwise change the field of view based on the change in distance between the touches.
+            camera.fieldOfView += deltaMagnitudeDiff * perspectiveZoomSpeed;
+
+            // Clamp the field of view to make sure it's between 0 and 180.
+            camera.fieldOfView = Mathf.Clamp(camera.fieldOfView, 0.1f, 179.9f);
+        }
+    }
 }
 
 static function ClampAngle (angle : float, min : float, max : float) {
