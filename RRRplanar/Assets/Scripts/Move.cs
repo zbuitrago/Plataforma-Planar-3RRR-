fb8165b27c04104bddac1101b258249580fcf389
AndresDiaz1 <andiazvar@gmail.com>
2015-05-05 16:41:32 -0500

    Ecuaciones completas (no sirven)

--- RRRplanar/Assets/Scripts/Move.cs
+++ RRRplanar/Assets/Scripts/Move.cs
@@ -20,24 +20,30 @@ public class Move : MonoBehaviour {
 	private float Xincrement=1.0f;
 	private float Zincrement=1.0f;
 	private float Lbrazo=1.15f; 			//Largo del brazo
-	private float LAntebrazo=1.0f; 		//Largo del Antebrazo
-	private float oy1=2.388299f; 			//posicion en y base 1
-	private float ox1=-3.492431f; 			//posicion en x base 1
+	private float LAntebrazo=1.0f; 			//Largo del Antebrazo
 	private float ni=0.8f;					//Distancia del punto M al punto P
-	private float phi=30.0f;
+	private float phi=3.20f;
 	private float[] Angles1;
 	private float[] Angles2;
 	private float[] Angles3;
-	private float initialtheta1=38.45f;
-	private float initialalpha1=57.57f;
+	private float initialtheta1=354.51f;
+	private float initialalpha1=80.53f;
+	private float initialtheta2=106.9f;
+	private float initialalpha2=188.68f;
+	private float initialtheta3=212.38f;
+	private float initialalpha3=334.82f;
 	private float angle;
 	private float DynamicTheta1;
 	private float DynamicAlpha1;
+	private float DynamicTheta2;
+	private float DynamicAlpha2;
+	private float DynamicTheta3;
+	private float DynamicAlpha3;
 	private int linkCounter;
 	
 	
-	public GameObject brazo1;
-	public GameObject Antebrazo1;
+	public GameObject brazo3;		//El brazo1 equivale al brazo 3  theta 3 en el PDF
+	public GameObject Antebrazo3;	//El Antebrazo1 equivale al Antebrazo 3 alpha 3 en el PDF
 	public Text Texto;
 	
 	
@@ -46,7 +52,8 @@ public class Move : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		basemovil = GameObject.Find ("ensamble/Ensamble - Brazo-1");
-		//brazo1=GameObject.Find ("ensamble/Ensamble - Brazo-1");
+		DynamicTheta3 = initialtheta3;
+		DynamicAlpha3 = initialalpha3;
 		isStarted = false;
 	}
 	
@@ -55,23 +62,40 @@ public class Move : MonoBehaviour {
 		
 		if(isStarted==true){
 			angle+=10*Time.deltaTime;
-			if(DynamicTheta1<=(Angles1[0])){
+
+			Debug.Log("Theta3 dinamico: "+DynamicTheta3.ToString());
+
+			if(DynamicTheta3>=Angles3[0]){
+				brazo3.transform.eulerAngles=new Vector3(0,angle,0);
+				float localAngle=brazo3.transform.eulerAngles.y;
+				DynamicTheta3=initialtheta3-localAngle;
+				Debug.Log("Theta3 dinamico: "+DynamicTheta3.ToString());
+			}
+
+			if(DynamicTheta3<=Angles3[0]){
+				brazo3.transform.eulerAngles=new Vector3(0,-angle,0);
+				float localAngle=brazo3.transform.eulerAngles.y;
+				DynamicTheta3=initialtheta3+localAngle;
+				Debug.Log("Theta3 dinamico: "+DynamicTheta3.ToString());
+			}
+			
+			/*if(DynamicTheta3<=(Angles3[0])){
 				//brazo1.transform.Rotate (new Vector3 (0, 10, 0) * Time.deltaTime);
-				brazo1.transform.eulerAngles=new Vector3(0,-angle,0);
-				DynamicTheta1=brazo1.transform.eulerAngles.y;
-				DynamicTheta1=((-1*(DynamicTheta1-360))+initialtheta1);
+				brazo3.transform.eulerAngles=new Vector3(0,-angle,0);
+				DynamicTheta3=brazo3.transform.eulerAngles.y;
+				DynamicTheta3=((-1*(DynamicTheta3-360))+initialtheta3);
 				//initialtheta1=+y;
 				//y=y/Mathf.PI*180;
-				Debug.Log("Angulo theta1: "+DynamicTheta1.ToString());
+				Debug.Log("Angulo theta3: "+DynamicTheta3.ToString());
 				
 			}
 			
-			if(DynamicAlpha1<=(-1*Angles1[1])){
-				Antebrazo1.transform.eulerAngles=new Vector3(0,angle,0);
-				DynamicAlpha1=Antebrazo1.transform.eulerAngles.y;
-				DynamicAlpha1=(DynamicAlpha1+initialalpha1);
-				Debug.Log(DynamicAlpha1);
-			}
+			if(DynamicAlpha3<=(-1*Angles3[1])){
+				Antebrazo3.transform.eulerAngles=new Vector3(0,angle,0);
+				DynamicAlpha3=Antebrazo3.transform.eulerAngles.y;
+				DynamicAlpha3=(DynamicAlpha3+initialalpha3);
+				Debug.Log(DynamicAlpha3);
+			}*/
 			
 			
 			
@@ -98,14 +122,23 @@ public class Move : MonoBehaviour {
 		float gamma1=210f;				//gammai Angulos entre horizontal y linea ni
 		float gamma2=-30f;
 		float gamma3=90f;
+
+		float oy1=5.539518f; 			//posicion en y brazo 2 que en realidad es e el brazo 1 theta1
+		float ox1=-1.4949f; 			//posicion en x brazo 2 que en realidad es e el brazo 1 theta1
+
+		float oy2=5.544016f; 			//posicion en y brazo 3 que en realidad es e el brazo 2 theta2
+		float ox2=-5.498871f; 			//posicion en x brazo 3 que en realidad es e el brazo 2 theta2
 		
-		Angles1=CalculateThethaAndAlpha (gamma1);
-		Angles2=CalculateThethaAndAlpha (gamma2);
-		Angles3=CalculateThethaAndAlpha (gamma3);
+		float oy3=2.388299f; 			//posicion en y brazo 1 que en realidad es e el brazo 3 theta3
+		float ox3=-3.492431f; 			//posicion en x brazo 1 que en realidad es e el brazo 3 theta3
+		
+		Angles1=CalculateThethaAndAlpha (gamma1, ox1, oy1);
+		Angles2=CalculateThethaAndAlpha (gamma2, ox2, oy2);
+		Angles3=CalculateThethaAndAlpha (gamma3, ox3, oy3);
 	}
 	
-	private float[] CalculateThethaAndAlpha(float gamma1){
-		
+	private float[] CalculateThethaAndAlpha(float gamma1, float ox1, float oy1){
+
 		linkCounter = linkCounter + 1;
 		
 		float bx1 = ni * Mathf.Cos ((gamma1*Mathf.PI/2)/90);	//ni*cos(gammai)
@@ -113,15 +146,15 @@ public class Move : MonoBehaviour {
 		float by1 = ni * Mathf.Sin ((gamma1*Mathf.PI/2)/90);	//ni*sin(gammai)
 		//Debug.Log ("by1=" + by1.ToString ());
 		
-		float Pyb = 3.1f;//spherePosition.z;
-		float Pxb = -3.5f;//spherePosition.x;
+		float Pyb = spherePosition.z;
+		float Pxb = spherePosition.x;
 		
-		float A1=2*LAntebrazo*(oy1-(bx1*Mathf.Sin((phi*Mathf.PI/2)/90))-(by1*Mathf.Cos((phi*Mathf.PI/2)/90))-Pyb);
+		float A1=2*Lbrazo*(oy1-(bx1*Mathf.Sin((phi*Mathf.PI/2)/90))-(by1*Mathf.Cos((phi*Mathf.PI/2)/90))-Pyb);
 		//Debug.Log ("A1=" + A1.ToString ());
-		float B1=2*LAntebrazo*(ox1+by1*Mathf.Sin((phi*Mathf.PI/2)/90)-bx1*Mathf.Cos((phi*Mathf.PI/2)/90)-Pxb);
+		float B1=2*Lbrazo*(ox1+by1*Mathf.Sin((phi*Mathf.PI/2)/90)-bx1*Mathf.Cos((phi*Mathf.PI/2)/90)-Pxb);
 		//Debug.Log ("B1=" + B1.ToString ());
 		
-		float C1 = -((Mathf.Pow (LAntebrazo, 2)) - (2 * Pyb * oy1) - (2 * Pxb * ox1) + (Mathf.Pow (bx1, 2) + Mathf.Pow (by1, 2) + Mathf.Pow (ox1, 2) + Mathf.Pow (oy1, 2) + Mathf.Pow (Pxb, 2) + Mathf.Pow (Pyb, 2) - Mathf.Pow (Lbrazo, 2)) +
+		float C1 = -((Mathf.Pow (Lbrazo, 2)) - (2 * Pyb * oy1) - (2 * Pxb * ox1) + (Mathf.Pow (bx1, 2) + Mathf.Pow (by1, 2) + Mathf.Pow (ox1, 2) + Mathf.Pow (oy1, 2) + Mathf.Pow (Pxb, 2) + Mathf.Pow (Pyb, 2) - Mathf.Pow (LAntebrazo, 2)) +
 		             ((2 * Mathf.Cos ((phi * Mathf.PI / 2) / 90)) * ((Pxb * bx1) + (Pyb * by1) - (bx1 * ox1) - (by1 * oy1))) + ((2 * Mathf.Sin ((phi * Mathf.PI / 2) / 90)) * ((Pyb * bx1) - (Pxb * by1) - (bx1 * oy1) + (by1 * ox1)))); //
 		//Debug.Log ("C1=" + C1.ToString ());
 		
@@ -137,7 +170,7 @@ public class Move : MonoBehaviour {
 		//Debug.Log ("Theta"+linkCounter.ToString()+" convertido: "+theta1.ToString());
 		//Debug.Log ("ThetaCodoAbajo"+linkCounter.ToString()+" convertido: "+theta1CodoAbajo.ToString());
 		
-		float Alpha=CalculateAlpha (theta1,Pxb,bx1,by1);
+		float Alpha=CalculateAlpha (theta1,Pxb,bx1,by1, ox1);
 		float[] Angles= new float[2];
 		Angles [0] = theta1;
 		Angles [1] = Alpha;
@@ -146,14 +179,14 @@ public class Move : MonoBehaviour {
 	}
 	
 	
-	private float CalculateAlpha(float theta1, float Pxb, float bx1, float by1)
+	private float CalculateAlpha(float theta1, float Pxb, float bx1, float by1, float ox1)
 	{
 		
 		float D1 = -Mathf.Sin(theta1*((Mathf.PI/2)/90));
 		//Debug.Log ("D1: "+D1.ToString());
 		float E1= Mathf.Cos(theta1*((Mathf.PI/2)/90));
 		//Debug.Log ("E1: "+E1.ToString());
-		float G1=(Pxb+(bx1*Mathf.Cos (phi*(Mathf.PI/2)/90))-(by1*Mathf.Sin (phi*(Mathf.PI/2)/90))-(ox1)-(Lbrazo*(Mathf.Cos (theta1*(Mathf.PI/2)/90))))/Lbrazo;
+		float G1=(Pxb+(bx1*Mathf.Cos (phi*(Mathf.PI/2)/90))-(by1*Mathf.Sin (phi*(Mathf.PI/2)/90))-(ox1)-(Lbrazo*(Mathf.Cos (theta1*(Mathf.PI/2)/90))))/LAntebrazo;
 		//Debug.Log ("G1 "+G1.ToString());
 		float Alpha1= (Mathf.Atan2 ((D1),(E1)))+(Mathf.Atan2((Mathf.Sqrt (Mathf.Pow(D1,2)+Mathf.Pow(E1,2)-Mathf.Pow(G1,2))),G1));
 		Alpha1=((Alpha1*90)/(Mathf.PI/2));
