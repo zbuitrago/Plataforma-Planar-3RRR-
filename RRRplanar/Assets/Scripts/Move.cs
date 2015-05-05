d481a2e49ef0f967fa1dd609eda92f22f25f2fe7
AndresDiaz1 <andiazvar@gmail.com>
2015-05-05 14:22:02 -0500

    Cambia ángulos a todos positivos

--- RRRplanar/Assets/Scripts/Move.cs
+++ RRRplanar/Assets/Scripts/Move.cs
@@ -3,15 +3,15 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class Move : MonoBehaviour {
-
+	
 	/// <summary>
 	/// Ecuaciones y variables de: http://cdn.intechopen.com/pdfs-wm/34391.pdf	/// 
 	/// </summary>
-
+	
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	/////LAS DISTANCIAS DE SOLIDWORKS A UNITY3D -> 500 UNIDADES EN SOLIDWORKS EQUIVALEN A 5 UNIDADES EN UNITY3D///
 	/// //////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+	
 	private GetPosition positionObject;
 	private Vector3 spherePosition;
 	private GameObject sphere;
@@ -26,21 +26,23 @@ public class Move : MonoBehaviour {
 	private float ni=0.8f;					//Distancia del punto M al punto P
 	private float phi=30.0f;
 	private float[] Angles1;
+	private float[] Angles2;
+	private float[] Angles3;
 	private float initialtheta1=38.45f;
 	private float initialalpha1=57.57f;
 	private float angle;
 	private float DynamicTheta1;
 	private float DynamicAlpha1;
 	private int linkCounter;
-
-
+	
+	
 	public GameObject brazo1;
 	public GameObject Antebrazo1;
 	public Text Texto;
-
-
-
-
+	
+	
+	
+	
 	// Use this for initialization
 	void Start () {
 		basemovil = GameObject.Find ("ensamble/Ensamble - Brazo-1");
@@ -50,7 +52,7 @@ public class Move : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+		
 		if(isStarted==true){
 			angle+=10*Time.deltaTime;
 			if(DynamicTheta1<=(Angles1[0])){
@@ -61,93 +63,92 @@ public class Move : MonoBehaviour {
 				//initialtheta1=+y;
 				//y=y/Mathf.PI*180;
 				Debug.Log("Angulo theta1: "+DynamicTheta1.ToString());
-
+				
 			}
-
+			
 			if(DynamicAlpha1<=(-1*Angles1[1])){
 				Antebrazo1.transform.eulerAngles=new Vector3(0,angle,0);
 				DynamicAlpha1=Antebrazo1.transform.eulerAngles.y;
 				DynamicAlpha1=(DynamicAlpha1+initialalpha1);
 				Debug.Log(DynamicAlpha1);
 			}
-
-
-
+			
+			
+			
 		}
-
-
+		
+		
 	}
-
+	
 	public void GetSpherePosition(){
-
+		
 		sphere=GameObject.Find ("sphere1");
 		spherePosition = sphere.transform.position;
-		Debug.Log (spherePosition.ToString());
-
+		
 		Texto.text = spherePosition.ToString ();
-
+		
 		CalculateThetaAngles ();
+		ConvertAnglesToPositive ();
 		isStarted = true;
-
-
+		
+		
 	}
-
+	
 	private void CalculateThetaAngles(){
 		float gamma1=210f;				//gammai Angulos entre horizontal y linea ni
 		float gamma2=-30f;
 		float gamma3=90f;
-
+		
 		Angles1=CalculateThethaAndAlpha (gamma1);
-		float[] Angles2=CalculateThethaAndAlpha (gamma2);
-		float[] Angles3=CalculateThethaAndAlpha (gamma3);
-
+		Angles2=CalculateThethaAndAlpha (gamma2);
+		Angles3=CalculateThethaAndAlpha (gamma3);
 	}
-
+	
 	private float[] CalculateThethaAndAlpha(float gamma1){
-
+		
 		linkCounter = linkCounter + 1;
-
+		
 		float bx1 = ni * Mathf.Cos ((gamma1*Mathf.PI/2)/90);	//ni*cos(gammai)
 		//Debug.Log ("bx1=" + bx1.ToString ());
 		float by1 = ni * Mathf.Sin ((gamma1*Mathf.PI/2)/90);	//ni*sin(gammai)
 		//Debug.Log ("by1=" + by1.ToString ());
-
-		float Pyb = spherePosition.z;
-		float Pxb = spherePosition.x;
-
+		
+		float Pyb = 3.1f;//spherePosition.z;
+		float Pxb = -3.5f;//spherePosition.x;
+		
 		float A1=2*LAntebrazo*(oy1-(bx1*Mathf.Sin((phi*Mathf.PI/2)/90))-(by1*Mathf.Cos((phi*Mathf.PI/2)/90))-Pyb);
 		//Debug.Log ("A1=" + A1.ToString ());
 		float B1=2*LAntebrazo*(ox1+by1*Mathf.Sin((phi*Mathf.PI/2)/90)-bx1*Mathf.Cos((phi*Mathf.PI/2)/90)-Pxb);
 		//Debug.Log ("B1=" + B1.ToString ());
-
+		
 		float C1 = -((Mathf.Pow (LAntebrazo, 2)) - (2 * Pyb * oy1) - (2 * Pxb * ox1) + (Mathf.Pow (bx1, 2) + Mathf.Pow (by1, 2) + Mathf.Pow (ox1, 2) + Mathf.Pow (oy1, 2) + Mathf.Pow (Pxb, 2) + Mathf.Pow (Pyb, 2) - Mathf.Pow (Lbrazo, 2)) +
-			((2 * Mathf.Cos ((phi * Mathf.PI / 2) / 90)) * ((Pxb * bx1) + (Pyb * by1) - (bx1 * ox1) - (by1 * oy1))) + ((2 * Mathf.Sin ((phi * Mathf.PI / 2) / 90)) * ((Pyb * bx1) - (Pxb * by1) - (bx1 * oy1) + (by1 * ox1)))); //
+		             ((2 * Mathf.Cos ((phi * Mathf.PI / 2) / 90)) * ((Pxb * bx1) + (Pyb * by1) - (bx1 * ox1) - (by1 * oy1))) + ((2 * Mathf.Sin ((phi * Mathf.PI / 2) / 90)) * ((Pyb * bx1) - (Pxb * by1) - (bx1 * oy1) + (by1 * ox1)))); //
 		//Debug.Log ("C1=" + C1.ToString ());
-
+		
 		float theta1 = (Mathf.Atan2 ((A1),(B1)))+(Mathf.Atan2((Mathf.Sqrt (Mathf.Pow(A1,2)+Mathf.Pow(B1,2)-Mathf.Pow(C1,2))),C1));
 		//Debug.Log ("heta 1 sin convt"+theta1.ToString());
 		theta1=((theta1*90)/(Mathf.PI/2));
-
+		
 		float theta1CodoAbajo = (Mathf.Atan2 ((A1),(B1)))-(Mathf.Atan2((Mathf.Sqrt (Mathf.Pow(A1,2)+Mathf.Pow(B1,2)-Mathf.Pow(C1,2))),C1));
 		//Debug.Log ("heta 1 sin convt"+theta1.ToString());
 		theta1CodoAbajo=((theta1CodoAbajo*90)/(Mathf.PI/2));
-
-
-		Debug.Log ("Theta"+linkCounter.ToString()+" convertido: "+theta1.ToString());
+		
+		
+		//Debug.Log ("Theta"+linkCounter.ToString()+" convertido: "+theta1.ToString());
 		//Debug.Log ("ThetaCodoAbajo"+linkCounter.ToString()+" convertido: "+theta1CodoAbajo.ToString());
-
+		
 		float Alpha=CalculateAlpha (theta1,Pxb,bx1,by1);
 		float[] Angles= new float[2];
 		Angles [0] = theta1;
 		Angles [1] = Alpha;
 		return Angles ;
-
+		
 	}
-
-
+	
+	
 	private float CalculateAlpha(float theta1, float Pxb, float bx1, float by1)
 	{
-
+		
 		float D1 = -Mathf.Sin(theta1*((Mathf.PI/2)/90));
 		//Debug.Log ("D1: "+D1.ToString());
 		float E1= Mathf.Cos(theta1*((Mathf.PI/2)/90));
@@ -156,8 +157,55 @@ public class Move : MonoBehaviour {
 		//Debug.Log ("G1 "+G1.ToString());
 		float Alpha1= (Mathf.Atan2 ((D1),(E1)))+(Mathf.Atan2((Mathf.Sqrt (Mathf.Pow(D1,2)+Mathf.Pow(E1,2)-Mathf.Pow(G1,2))),G1));
 		Alpha1=((Alpha1*90)/(Mathf.PI/2));
-		Debug.Log ("Alpha"+linkCounter.ToString()+" convertido: "+Alpha1.ToString());
+		//Debug.Log ("Alpha"+linkCounter.ToString()+" convertido: "+Alpha1.ToString());
 		return Alpha1;
+		
+	}
+	
+	private void ConvertAnglesToPositive(){
+		float[] AllAngles= new float[6];
+		AllAngles=AssignAnglesXtoAllAngles (AllAngles);
+		
+		for(int i=0; i<=5; i++){
+			if(AllAngles[i]<0){
+				AllAngles[i]=AllAngles[i]+360;
+			}
+		}
+		
+		AssignAllAnglestoAnglesX (AllAngles);
+		DebugLogAngles (AllAngles);
+	}
+	
+	private float[] AssignAnglesXtoAllAngles (float[] AllAngles)
+	{
+		AllAngles [0] = Angles1 [0];
+		AllAngles [1] = Angles2 [0];
+		AllAngles [2] = Angles3 [0];
+		AllAngles [3] = Angles1 [1];
+		AllAngles [4] = Angles2 [1];
+		AllAngles [5] = Angles3 [1];
+		return AllAngles;
+	}
+	
+	private void AssignAllAnglestoAnglesX (float[] AllAngles)
+	{
+		Angles1 [0] = AllAngles [0];
+		Angles2 [0] = AllAngles [1];
+		Angles3 [0] = AllAngles [2];
+		Angles1 [1] = AllAngles [3];
+		Angles2 [1] = AllAngles [4];
+		Angles3 [1] = AllAngles [5];
+	}
+	
+	private void DebugLogAngles (float[] AllAngles)
+	{
+		Debug.Log("Theta1 "+ AllAngles[0].ToString());
+		Debug.Log("Theta2 "+ AllAngles[1].ToString());
+		Debug.Log("Theta3 "+ AllAngles[2].ToString());
+		
+		Debug.Log("Alpha1 "+ AllAngles[3].ToString());
+		Debug.Log("Alpha2 "+ AllAngles[4].ToString());
+		Debug.Log("Alpha3 "+ AllAngles[5].ToString());
 		
 	}
 }
